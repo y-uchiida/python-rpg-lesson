a55885217c79e6aecb5e9f5296b7efbb9111ea30
y-uchiida <y.uchiida@nirneege.com>
2025-04-01 13:12:18 +0000

    docs(Document): :memo: 開発パート03 を作成

--- docs/rdd/src/constants/SIDEBAR.ts
+++ docs/rdd/src/constants/SIDEBAR.ts
@@ -11,4 +11,8 @@ export const SIDEBAR = [
     text: '開発パート02: 戦闘と回復を繰り返して、ゲームを継続して行えるようにする',
     link: '/lessons/lesson-02',
   },
+  {
+    text: '開発パート03: キャラクターが戦うプログラムを作成する',
+    link: '/lessons/lesson-03',
+  },
 ];
