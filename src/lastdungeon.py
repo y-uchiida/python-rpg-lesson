d6f17e0c11ef7a060064588b638b1d63dd58f45b
y-uchiida <y.uchiida@nirneege.com>
2025-04-05 07:50:18 +0000

    Lesson-06: LastDungeon クラスを作成

---
+++ src/lastdungeon.py
@@ -0,0 +1,9 @@
+from enemymonster import EnemyMonster
+from field import Field
+
+class LastDungeon(Field):
+    def __init__(self, enemies):
+        super().__init__(enemies)
+        # ラスボスキャラクターを追加
+        boss = EnemyMonster(name="ラスボス", hp=200, max_hp=200, strength=30, defense=10)
+        self.enemies.append(boss)
